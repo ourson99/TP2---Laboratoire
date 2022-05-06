4e014715d3ac14571fefa1a1d3fc2650dd7101e6
Francis Lemay <francis.belanger.lemay@gmail.com>
2022-05-05 20:38:58 -0400

    Add 12312

--- Sources/labo.h
+++ Sources/labo.h
@@ -25,26 +25,6 @@ void queue_init(Queue* q);
 void queue_push(Queue* q, void* data);
 void* queue_pop(Queue* q);
 
-/*
-* Programmer l'algorithme de fibonacci.
-* Faire attention lorsque vous utilisez un type pour une variable qui représente un nombre, int ne dépasse pas 2^31-1 ou 2,147,483,647
-*/
-long long fibonacci(int n);
-
-/*
-* Programmer l'algorithme de fibonacci avec la memoization en utilisant une variable static fib_cache, implémenter dans la fonction. Son type sera long long [].
-* Utiliser OPTICK_EVENT(); pour enregistrer la fonction dans le profiler
-* Faire attention lorsque vous utilisez un type pour une variable qui représente un nombre, int ne dépasse pas 2^31-1 ou 2,147,483,647
-*/
-long long fibonacci_memoization(int n);
-
-/*
-* Programmer l'algorithme de fibonacci avec la memoization en utilisant une variable static fib_cache, implémenter dans la fonction. Son type sera long long**.
-* Allouer de la memoire pour fib_cache en utilisant allocate. Après avoir trouver un resultat, pour le mettre dans fib_cache, allouer un int* avec malloc puis ajouter le a fib_cache.
-* Utiliser OPTICK_EVENT(); pour enregistrer la fonction dans le profiler
-* Faire attention lorsque vous utilisez un type pour une variable qui représente un nombre, int ne dépasse pas 2^31-1 ou 2,147,483,647.
-*/
-long long fibonacci_memoization_malloc(int n);
 
 
 
@@ -83,17 +63,3 @@ void add_node(AdjMatrix* graph, void* data, Vector2 pos);
 */
 void add_edge(AdjMatrix* graph, int fromNode, int toNode, uint8_t cost);
 
-
-/*
-* Construire les groupes de nodes
-* Utiliser OPTICK_EVENT(); pour enregistrer la fonction dans le profiler
-*/
-//void build_groups(AdjMatrix* graph);
-
-/*
-* Aller chercher votre implémentation d'astar. Faire des test de profiling avec votre implémentation. 
-* Essayer de changer le packing de la struct Node et voir si cela a un impact.
-* Essayer d'utiliser la fonction avec build_groups et sans build_groups
-* Utiliser OPTICK_EVENT(); pour enregistrer la fonction dans le profiler
-*/
-//void astar(AdjMatrix* graph,int startNodeIndex, int endNodeIndex, Stack* solvedPath);
